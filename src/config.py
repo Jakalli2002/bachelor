6ca5f2808e2ccc938c9a602d1c96b22fd03793a6
Julius Schuhmann <171662436+Jakalli2002@users.noreply.github.com>
2026-07-22 22:23:00 +0200

    added: config.py. venv: torch, transformers

---
+++ src/config.py
@@ -0,0 +1,30 @@
+# -------------------- CONFIG --------------------
+# general configuration for all modules
+# four groups: PATHS, EXPERIMENT DESIGN, CLASSIC, BERT
+# PATHS : where training- and testdata is located, where to put results and graphs
+# EXPERIMENT DESIGN: which % to run, how many repetitions, seed control
+# CLASSIC: config for the classic model (log. reg.)
+# BERT: config for the Bert model
+
+
+
+
+# -------------------- PATHS --------------------
+
+
+
+
+# -------------------- EXPERIMENT DESIGN --------------------
+
+
+
+
+# -------------------- CLASSIC --------------------
+
+
+
+
+# -------------------- BERT --------------------
+
+
+
