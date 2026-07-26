365fd04efb45876edfc1bd5965969106ab8e7fce
Julius Schuhmann <171662436+Jakalli2002@users.noreply.github.com>
2026-07-26 16:16:26 +0200

    updates requirements, added subset function in data.py

--- src/data.py
+++ src/data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from pathlib import Path
 from config import TRAINING_PATH, TEST_PATH
 
@@ -18,10 +19,23 @@ def split_csv(path: Path) -> list:
 
 
 # -------------------- LOAD DATA --------------------
+# load data from the 10kgnad csv into a dataframe
 def load_data(path: Path) -> pd.DataFrame:
     ordered_data = split_csv(path)
     df = pd.DataFrame(ordered_data, columns=["category", "text"])
     return df
+
+
+# -------------------- GET SUBSET OF TRAINING-DATA --------------------
+# creates a subset of the trainigdata dataframe (10%, 20%...) for the model training with a given percentage of the traininddata
+# uses stratify to keep the category distribution
+# uses a seed to ensure reproducibility
+def get_data_subset(size: float, seed: int) -> pd.DataFrame:
+    """
+    
+    """
+
+
 
 
 # -------------------- INIT DF --------------------
@@ -34,3 +48,14 @@ print("--- TRAIN-DATA ---")
 print(train_df)
 print("\n--- TEST-DATA ---")
 print(test_df)
+
+
+# -------------------- STATS --------------------
+print("\n-- TRAIN-DATA-CATEGORY-DIST --")
+print(train_df["category"].value_counts())
+print("\n-- TEST-DATA-CATEGORY-DIST --")
+print(test_df["category"].value_counts())
+print("\n-- TRAIN-DATA-ARTICLE-LENGTH-MEAN-(CHARACTERS) --")
+print(round(train_df["text"].str.len().mean()))
+print("\n-- TEST-DATA-ARTICLE-LENGTH-MEAN-(CHARACTERS) --")
+print(round(test_df["text"].str.len().mean()))
